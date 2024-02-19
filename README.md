27e5a6b23d1823d07db42ca138292ca4833b533c
vincent-the-gamer <1127620453@qq.com>
2024-02-19 08:55:50 +0800

    chore: init.

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1 align="center">Nemahaizai</h1>
+
+<p align="center">
+  Transform netease <b>.ncm</b> audios into <b>mp3/flac</b> and calculate audio BPM.
+</p>
+
+# Notice
+Under construction.
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig(async () => ({
+  plugins: [vue()],
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 8080,
+    strictPort: true,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
